b94c55d6f445b4ad420f00df5b406042bb1a8b4c
Suvarna <sumanthkadri.suvarna@monster.com>
2019-07-25 21:14:21 +0800

    member controller changes

--- server/api/member/memberController.js
+++ server/api/member/memberController.js
@@ -1,35 +1,40 @@
 var Member = require('./memberModel');
+var Organisation = require('../organisation/organisationModel');
 var _ = require('lodash');
 var logger = require('../../util/logger');
 
 exports.get = function (req, res, next) {
-
-    Member.find({})
-        .then(function (members) {
-            console.log(members)
-            res.json(members);
+    Organisation.findOne({ orgname: req.orgname })
+        .then(function (org) {
+            return org.members
         }, function (err) {
             next(err);
+        }).then(function (members) {
+            res.json(members);
         });
 };
 
 exports.post = function (req, res, next) {
     var newMember = req.body;
-
-    Member.create(newMember)
-        .then(function (member) {
-            res.json(member);
+    Organisation.findOne({ orgname: req.orgname })
+        .then(function (org) {
+            var members = org.members;
+            members.push(newMember)
+            org.save()
+            res.json(newMember);
         }, function (err) {
             next(err);
-        });
+        })
 };
 
 exports.delete = function (req, res, next) {
-    Member.remove({})
-        .then(function (members) {
-            console.log(members)
-            res.json(members);
+
+    Organisation.findOne({ orgname: req.orgname })
+        .then(function (org) {
+            org.members = [];
+            org.save()
+            res.json(org.members);
         }, function (err) {
             next(err);
-        });
+        })
 };
